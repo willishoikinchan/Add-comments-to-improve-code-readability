d0769f4cb9dbb84db474136f475ba41613a2e9a4
willishoikinchan <161003707+willishoikinchan@users.noreply.github.com>
2024-02-23 07:04:09 +0000

    Added comments to improve code readability

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+# helloWorld.py
+
+def main():
+    # Get user input
+    user_input = input("hello World!")
+    
+    # Print the user input
+    print("hello World!", user_input)
+
+if __name__ == "__main__":
+    main()
